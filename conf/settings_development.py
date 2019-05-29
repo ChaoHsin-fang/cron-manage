820d35ecc16debac9cdbfed0c03e32e5122ada0f
zhaoxinfang <zhaoxinfang@beyondcent.com>
2019-05-29 18:14:59 +0800

    修改配置文件

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -20,35 +20,35 @@ import logging
 # }
 
 # 测试环境数据库
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-#         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-#         'USER': 'root',                        # 你的数据库user
-#         'PASSWORD': '123456',                        # 你的数据库password
-#         'HOST': '10.20.11.119',                   # 开发的时候，使用localhost
-#         'PORT': '3306',                        # 默认3306
-#     },
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                        # 你的数据库user
+        'PASSWORD': '123456',                        # 你的数据库password
+        'HOST': '10.20.11.119',                   # 开发的时候，使用localhost
+        'PORT': '3306',                        # 默认3306
+    },
+}
 
 
 # ===============================================================================
 # 数据库设置, 正式环境数据库设置
 # # ===============================================================================
-DB_USERNAME = os.environ.get('BKAPP_DB_USERNAME', '')
-DB_PASSWORD = os.environ.get('BKAPP_DB_PASSWORD', '')
-DB_HOST = os.environ.get('BKAPP_DB_HOST', '')
-DB_PORT = os.environ.get('BKAPP_DB_PORT', '')
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,  # 数据库名 (默认与APP_ID相同)
-        'USER': os.environ.get('DB_USERNAME', DB_USERNAME),
-        'PASSWORD': os.environ.get('DB_PASSWORD', DB_PASSWORD),
-        'HOST': os.environ.get('DB_HOST', DB_HOST),
-        'PORT': os.environ.get('DB_PORT', DB_PORT),
-    }
-}
-
-logging.info(APP_ID)
+# DB_USERNAME = os.environ.get('BKAPP_DB_USERNAME', '')
+# DB_PASSWORD = os.environ.get('BKAPP_DB_PASSWORD', '')
+# DB_HOST = os.environ.get('BKAPP_DB_HOST', '')
+# DB_PORT = os.environ.get('BKAPP_DB_PORT', '')
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+#         'NAME': APP_ID,  # 数据库名 (默认与APP_ID相同)
+#         'USER': os.environ.get('DB_USERNAME', DB_USERNAME),
+#         'PASSWORD': os.environ.get('DB_PASSWORD', DB_PASSWORD),
+#         'HOST': os.environ.get('DB_HOST', DB_HOST),
+#         'PORT': os.environ.get('DB_PORT', DB_PORT),
+#     }
+# }
+#
+# logging.info(APP_ID)
